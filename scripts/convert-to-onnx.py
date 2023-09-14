eab0dd4730e83f15110d20102070633dcbaf80cd
opassos <adrianoutfpr@gmail.com>
2023-09-14 12:39:22 -0300

    added some flexibility

--- scripts/convert-to-onnx.py
+++ scripts/convert-to-onnx.py
@@ -16,19 +16,29 @@ class Wrapper(torch.nn.Module):
         self.model = model
 
     def forward(self, tensor):
-        ff = self.model(tensor)
-        return ff
+        outs = self.model.forward_features(tensor)
+        print(outs.keys())
+        return outs["x_norm_clstoken"], outs["x_norm_patchtokens"]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_name", type=str, default="dinov2_vits14", help="dinov2 model name")
 parser.add_argument(
-    "--image_height", type=int, default=280, help="input image height, must be a multiple of patch_size"
+    "--image_height", type=int, default=504, help="input image height, must be a multiple of patch_size"
 )
 parser.add_argument(
-    "--image_width", type=int, default=280, help="input image height, must be a multiple of patch_size"
+    "--image_width", type=int, default=504, help="input image height, must be a multiple of patch_size"
 )
 parser.add_argument(
-    "--patch_size", type=int, default=14, help="dinov2 model patch size, default is 16"
+    "--patch_size", type=int, default=14, help="dinov2 model patch size, default is 14"
+)
+parser.add_argument(
+    "--batch_size", type=int, default=1, help="dinov2 model batch size, default is 1"
+)
+parser.add_argument(
+    "--opset_version", type=int, default=14, help="onnx opset version, default is 14"
+)
+parser.add_argument(
+    "--output_name", type=str, default='', help="output onnx model name, default is model_name.onnx"
 )
 args = parser.parse_args()
 
@@ -41,12 +51,20 @@ if __name__ == "__main__":
     model = Wrapper(hubconf.dinov2_vits14(for_onnx=True)).to("cpu")
     model.eval()
 
-    dummy_input = torch.rand([1, 3, args.image_height, args.image_width]).to("cpu")
+    dummy_input = torch.rand([args.batch_size, 3, args.image_height, args.image_width]).to("cpu")
     dummy_output = model(dummy_input)
+    output_name = (
+        args.output_name 
+        if args.output_name 
+        else f"{args.model_name}_s{args.image_height}_bs{args.batch_size}_op{args.opset_version}.onnx"
+    )
 
     torch.onnx.export(
         model,
         dummy_input,
-        args.model_name + ".onnx",
-        input_names = ["input"]
+        output_name,
+        input_names = ["input"],
+        output_names = ["cls"],
+        opset_version=args.opset_version,
+        do_constant_folding=True,
     )
